9ac40b6ac97ea033dc1e0627b006288b40b91657
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2019-02-19 15:17:45 +0100

    update copyright notice

--- shell.cpp
+++ shell.cpp
@@ -1,5 +1,5 @@
 /*
-shell Copyright (c) 2013-2018 Jeremy Bernstein and Bill Orcutt
+shell Copyright (c) 2013-2019 Jeremy Bernstein and Bill Orcutt
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
